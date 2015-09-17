ace318dac94c7b2989374d81a820d6edf2a7f837
genuinearizona <timothyflores@hotmail.com>
2015-09-17 12:35:20 -0700

    js/app.js

--- js/app.js
+++ js/app.js
@@ -24,11 +24,22 @@ $(document).ready(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
     });
-	})
+	});
 function playHadouken() {
         $('#hadouken-sound')[0].volume = 0.5;
         $('#hadouken-sound')[0].load();
         $('#hadouken-sound')[0].play();
  }
-
+  $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-action').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  }
+          ); 
 
