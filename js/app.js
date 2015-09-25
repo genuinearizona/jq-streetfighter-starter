002c5e17450649b7acf6ebf5e718e5dd4f7d2222
genuinearizona <timothyflores@hotmail.com>
2015-09-25 14:24:34 -0700

    js/app.js

--- js/app.js
+++ js/app.js
@@ -6,15 +6,9 @@ $(document).ready(function() {
   })
   .mouseleave(function() {
     // mouse over changes Ryu from an animated state to immobile position //
-<<<<<<< HEAD
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-=======
     $('.ryu-hide').hide();
     $('.ryu-still').show();
-})
->>>>>>> 5380e6d77116f121a9d63248d1cf423d7da017a5
+  })
   .mousedown(function(){
   playHadouken();
    // mouse click shoots an animated blue fireball a hadouken to the left of the Ryu character //
@@ -33,7 +27,7 @@ $('.ryu-hide').hide();
 $('.ryu-ready').show();
 });
 });
-// Ryu says hadouken as he's throwing his hadouken //
+// ryu exlaims hadouken as he throws his animte fire ball hadouken //
 function playHadouken() {
         $('#hadouken-sound')[0].volume = 0.5;
         $('#hadouken-sound')[0].load();
@@ -43,18 +37,13 @@ $(document).keydown(function(e) {
         if (e.keyCode == 88) {
             // pressing the x alphabet on the keyboard move Ryu physical status to an arms folded postition //
             $('.ryu-hide').hide();
-            $('.ryu-cool').show();
+            $('.ryu-cool').show(); 
         }
     })
     $(document).keyup(function(e) {
         if (e.keyCode == 88) {
-<<<<<<< HEAD
-            $('.ryu-cool').hide();
-            $('.ryu-ready').show();    
-=======
             $('.ryu-hide').hide();
             $('.ryu-ready').show();
->>>>>>> 5380e6d77116f121a9d63248d1cf423d7da017a5
         }
         });
 
